6139eacc9450dd759806db095f0aaa87145a239d
Lucas <boxlucasbasso@gmail.com>
2024-11-03 22:22:40 -0600

    terminado

--- main.js
+++ main.js
@@ -1,56 +1,69 @@
-
-const GatitoNombre = []
+const GatitoNombre = [];
 
 const nombreDeGato = (nombre, porciones) => {
     GatitoNombre.push({
         nombre,
-        porciones   
-    })
-}
+        porciones
+    });
+};
 
+const nombreGatos = () => {
+    const NombreGatitos = GatitoNombre.map(gatito => gatito.nombre);
+    return NombreGatitos;
+};
 
-    let gatitos = Number(prompt('Veterinaria de gatitos\n Elige el gatito que mas te guste para darle alimento:\n 1- Gato blanco\n 2- Gato Anaranjado\n 3- Gato negro\n 4- Gato bicolor\n 5- Gato premium'))
-      let bandera = true
-
-    while(bandera)  
-        switch(gatitos){
-            case 1:
-                 let nombre = prompt("Usted escogio a su gato blanco\n ¿como quiere nombrar a su gato")
-                 let porciones = Number(prompt("cuantas porciones desea darle a" + ' ' +nombre))    
-            nombreDeGato(nombre, porciones)
-
-            bandera = confirm("volver?")
-        break
-            case 2:  
-                nombre = prompt("Usted escogio a su gato blanco\n ¿como quiere nombrar a su gato")
-               
-                porciones = Number(prompt("cuantas porciones desea darle a" + ' ' +nombre))    
-            nombreDeGato(nombre, porciones)
-        break
-            case 3:
-                nombre = prompt("Usted escogio a su gato blanco\n ¿como quiere nombrar a su gato")
-               
-                porciones = Number(prompt("cuantas porciones desea darle a" + ' ' +nombre))    
-            nombreDeGato(nombre, porciones)
-        break
-            case 4:
-                nombre = prompt("Usted escogio a su gato blanco\n ¿como quiere nombrar a su gato")
-               
-                porciones = Number(prompt("cuantas porciones desea darle a" + ' ' +nombre))    
-            nombreDeGato(nombre, porciones)
-        break
-            case 5:
-                nombre = prompt("Usted escogio a su gato premium\n Este gato ya tiene nombre") 
-
-                Bandera = confirm("No necesita ser alimentado")
-        break
-    default:
-            alert("podrias salvar un gatito")
-                bandera = confirm("¿quieres intentarlo?")
-                if (!bandera){
-                    alert("Los gatitos gatitos estan tristes")
-                }
-                gatitos = Number(prompt(" Veterinaria de gatitos\n Elige el gatito que mas te guste para darle alimento:\n 1- Gato blanco\n 2- Gato Anaranjado\n 3- Gato negro\n 4- Gato bicolor\n 5- Gato premium"))  
+const cambiarNombre = () => {
+    if (GatitoNombre.length < 1) {
+        alert("No hay gatitos");
+    } else {
+        const NombreGatitos = nombreGatos();
+        console.log(NombreGatitos);
+        
+        const i = Number(prompt("Elige el índice del gato que quieres renombrar (0 para el primero):"));
+        if (i >= 0 && i < GatitoNombre.length) {
+            const nuevoNombre = prompt("Introduce el nuevo nombre para el gato:");
+            GatitoNombre[i].nombre = nuevoNombre;
+            console.log("Nombre cambiado a:", nuevoNombre);
+        } else {
+            alert("Índice no válido");
+        }
     }
+};
 
-sistems ()
+const sistems = () => {
+    let bandera = true;
+
+    while (bandera) {
+        let gatitos = Number(prompt('Veterinaria de gatitos\nElige el gatito que más te guste para darle alimento:\n1- Gato blanco\n2- Gato Anaranjado\n3- Gato negro\n4- Gato bicolor\n5- Gato premium\n6- Cambiar nombre'));
+
+        switch (gatitos) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                const nombre = prompt(`Usted escogió a su gato ${["blanco", "anaranjado", "negro", "bicolor"][gatitos - 1]}\n¿Cómo quiere nombrar a su gato?`);
+                let porciones;
+                do {
+                    porciones = Number(prompt(`¿Cuántas porciones desea darle a ${nombre}?`));
+                }
+                 while (isNaN(porciones) || porciones <= 0);
+                nombreDeGato(nombre, porciones);
+                break;
+            case 5:
+                alert("Usted escogió a su gato premium\nEste gato ya tiene nombre");
+                break;
+            case 6:
+                cambiarNombre();
+                break;
+            default:
+                alert("Podrías salvar un gatito");
+                bandera = confirm("¿Quieres intentarlo?");
+                if (!bandera) {
+                    alert("Los gatitos están tristes");
+                }
+        }
+        bandera = confirm("¿Volver?");
+    }
+};
+
+sistems();
